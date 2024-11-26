d915b7f4c9c52d471123429f04b17757a326a948
MilaSayuri <sayurimatsuda276@gmail.com>
2024-11-26 18:24:25 -0300

    Add files via upload

---
+++ FlashCard/perguntas.js
@@ -0,0 +1,95 @@
+criaCartao (
+    'Programação',
+    'O que é JavaScript?',
+    'JavaScript é uma linguagem de programação.'
+)
+
+criaCartao (
+    'Programação',
+    'O que é CSS?',
+    'O CSS é uma linguagem de estilização.'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da Franca é Paris.'
+)
+
+criaCartao (
+    'Geografia',
+    'Quais são os seis biomas do Brasil?',
+    'Eles são o Amazônia, Cerrado, Caatinga, Mata Atlântica, Pantanal e Pamba.'
+)
+
+criaCartao (
+    'Geografia',
+    'Quais são os cinco biomas principais do mundo?',
+    'Eles são Taiga, Floresta Temperada, Savanas, Deserto e Tundra.'
+)
+
+criaCartao (
+    'Literatura',
+    'O que foi o movimento literário Barroco?',
+    'Idade Média, na Itália, linguagem rebuscada, conflito entre pecado e salvação,homem perante a igreja.'
+)
+
+criaCartao (
+    'Literatura',
+    'O que foi o movimento literário Arcadismo?',
+    'Durante o iluminismo,revolução francesa e inglesa, linguagen simples e elementos da natureza, amor galente nos versos e presença de pastores nos textos.'
+)
+
+criaCartao (
+    'Química',
+    'Quais são as sete camadas eletrônicas e quantos elétrons cada ma suporta?',
+    'Elas são K=2, L=8, M=18, N=32, O=32, P=18 e Q=8.'
+)
+
+criaCartao (
+    'Biologia',
+    'Quais são os seis sistemas que compõem o corpo humano?',
+    'Sistema Respiratório, Excretor, Nervoso, Reprodutor Feminino e reprodutor Masculino.'
+)
+
+criaCartao (
+    'Biologia',
+    'Qual a principal diferença entre mitose e meiose?',
+    'A mitose gera filhas identicas a mãe e a meiose gera filhas diferentes da mãe e uma da outra.'
+)
+
+criaCartao (
+    'Matemática',
+    'Qual a fórmula da PA?',
+    'An= A1+(n-1).R para n>/=2'
+)
+
+criaCartao (
+    'Matemática',
+    'Qual a fórmula da PG?',
+    'Sn= a1.(q^(n)-1)/q-1 para q diferente de 1'
+)
+
+criaCartao (
+    'Matemática',
+    'Quais são os tipos de análise combinatória?',
+    'Elas são Permutação simples e com repetição, Arranjo simples e com repetição e Combinação simples.'
+)
+
+criaCartao (
+    'Física',
+    'Qual a fórmula de dilatação linear?',
+    '(Variação)L= L(inicial).alfa.(variação)T'
+)
+
+criaCartao (
+    'Física',
+    'Quais fórmulas são necessárias para chega à energia mecânica?',
+    'Emec=Ec+Ep, Ec=m.v^(2)/2 e Ep=m.g.h'
+)
+
+criaCartao (
+    'Física',
+    'Qual a fórmula da conversão de escalas?',
+    'Tc/5 = Tf-32/9 = Tk-273/5'
+)
