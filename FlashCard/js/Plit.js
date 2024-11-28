d4160a1f4401973fb02999551d1ec2d020f254f4
MilaSayuri <sayurimatsuda276@gmail.com>
2024-11-28 13:31:30 -0300

    Add files via upload

---
+++ FlashCard/js/Plit.js
@@ -0,0 +1,11 @@
+criaCartao (
+    'Literatura',
+    'O que foi o movimento literário Barroco?',
+    'Idade Média, na Itália, linguagem rebuscada, conflito entre pecado e salvação,homem perante a igreja.'
+)
+
+criaCartao (
+    'Literatura',
+    'O que foi o movimento literário Arcadismo?',
+    'Durante o iluminismo,revolução francesa e inglesa, linguagen simples e elementos da natureza, amor galente nos versos e presença de pastores nos textos.'
+)
---
+++ FlashCard/js/Pmat.js
@@ -0,0 +1,17 @@
+criaCartao (
+    'Matemática',
+    'Qual a fórmula da PA?',
+    'An= A1+(n-1).R para n>/=2'
+)
+
+criaCartao (
+    'Matemática',
+    'Qual a fórmula da PG?',
+    'Sn= a1.(q^(n)-1)/q-1 para q diferente de 1'
+)
+
+criaCartao (
+    'Matemática',
+    'Quais são os tipos de análise combinatória?',
+    'Elas são Permutação simples e com repetição, Arranjo simples e com repetição e Combinação simples.'
+)
